0d62101f910db6016033eef6aa2a9731d08be8fe
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-07-15 16:24:47 +0400

    commit

--- script.js
+++ script.js
@@ -1,75 +1,103 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-const submitButton = document.getElementById('button');
+const form = document.getElementById("form");
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
 
-
-
-
- function showError(input, message) {
-    const formControl = input.parentElement;
-     formControl.className = 'form-control error';
-     input.nextElementSibling.innerText = message;
-     input.style.borderColor = "red";
- }
-
-
- function showSuccess(input) {
-     input.nextElementSibling.innerText = "";
-     input.style.borderColor = "green";
- }
-
-
- function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
- }
-
-
- function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-  } else if (input.value.length > max) {
-    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-  } else {
-    showSuccess(input);
-  }
-}
-
-function checkPasswordsMatch(input1, input2) {
-    if (input1.value !== input2.value) {
-      showError(input2, 'Confirm Password does not match');
-    }
-  }
-
-  function checkEmail(input) {
-    const regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if (regex.test(input.value.trim())) {
-      showSuccess(input);
-    } else {
-      showError(input, 'Email is not valid');
-    }
-  }
-
-
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
-
-
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  checkRequired([username, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
-  checkEmail(email);
-  checkPasswordsMatch(password, password2);
+  validateInputs();
 });
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorText = inputControl.querySelector(".error");
+  errorText.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorText = inputControl.querySelector(".error");
+  errorText.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const reset = () => {
+    userName.value='';
+    email.value='';
+    password.value='';
+    confirmPassword.value='';
+
+    userName.style.borderColor = "rgba(9, 9, 9, 0.359)";
+    email.style.borderColor = "rgba(9, 9, 9, 0.359)";
+    password.style.borderColor = "rgba(9, 9, 9, 0.359)";
+    confirmPassword.style.borderColor = "rgba(9, 9, 9, 0.359)";
+
+}
+
+const validateEmail = (email) => {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+};
+
+const validateInputs = () => {
+  const userNameValue = userName.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const confirmPasswordValue = confirmPassword.value;
+
+  let inputValid = true;
+
+  if (userNameValue === "") {
+    setError(userName, "Username is required");
+    inputValid = false;
+  } else if (userNameValue.length < 3 || userNameValue.length > 15) {
+    setError(userName, "Username must be at least 3 characters.");
+    inputValid = false;
+  } else {
+    setSuccess(userName);
+  }
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+    inputValid = false;
+  } else if (!validateEmail(emailValue)) {
+    setError(email, "Email is not valid.");
+    inputValid = false;
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordValue === "") {
+    setError(password, "Password is required");
+    inputValid = false;
+  } else if (passwordValue.length < 6) {
+    setError(password, "Password must be at least 6 characters.");
+    inputValid = false;
+  } else {
+    setSuccess(password);
+  }
+
+  if (confirmPasswordValue === "") {
+    setError(confirmPassword, "Password is required");
+    inputValid = false;
+  } else if (confirmPasswordValue !== passwordValue) {
+    setError(confirmPassword, "Password is required");
+    inputValid = false;
+  } else {
+    setSuccess(confirmPassword);
+  }
+
+  if(inputValid){
+    
+    const user = `<div class="box"><span>Username:</span> ${userNameValue} <br> <span>Email:</span> ${emailValue} <br> <span>Password:</span> ${passwordValue}</div>`;
+    const usersDiv = document.querySelector('.users');
+    usersDiv.innerHTML += user;
+    
+    reset();
+
+  }
+};
